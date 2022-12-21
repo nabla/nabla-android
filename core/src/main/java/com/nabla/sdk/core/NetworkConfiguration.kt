8345dbbb20d8e8c728bc63856cdc52e2b7cd5f31
Vincent Brison <vincent@nabla.com>
2022-12-21 17:05:37 +0100

    [Android] Ensure base url ends with / (#23875)
    * Ensure base url ends with /
    
    * Fix opt in
    
    Don't make network conf internal as it is part of the public init methods, and disallow e2e to use internal api.
    
    * Api dump

--- core/src/main/java/com/nabla/sdk/core/NetworkConfiguration.kt
+++ core/src/main/java/com/nabla/sdk/core/NetworkConfiguration.kt
@@ -1,5 +1,7 @@
 package com.nabla.sdk.core
 
+import com.nabla.sdk.core.domain.entity.InternalException.Companion.throwNablaInternalException
+
 /**
  * Nabla SDK Network configuration parameters.
  * This is exposed for internal usage and you should not have to use it in your app.
@@ -7,7 +9,13 @@ package com.nabla.sdk.core
  * @param baseUrl Optional — base url for Nabla API server.
  * @param additionalHeadersProvider Optional — useful to append additional query headers to http calls.
  */
-public class NetworkConfiguration(
+public data class NetworkConfiguration(
     internal val baseUrl: String = "https://api.nabla.com/",
     internal val additionalHeadersProvider: HeaderProvider? = null,
-)
+) {
+    init {
+        if (baseUrl.last() != '/') {
+            throwNablaInternalException("baseUrl($baseUrl) must end with a /")
+        }
+    }
+}
