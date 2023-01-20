6cb71945ddd392d4ef446ea5ce67dbe7c3d9812b
Benoit Letondor <benoitletondor@users.noreply.github.com>
2023-01-20 13:29:21 +0100

    Android SDK - Make StringOrRes part of the public API (#24400)

--- core/src/main/java/com/nabla/sdk/core/domain/entity/StringOrRes.kt
+++ core/src/main/java/com/nabla/sdk/core/domain/entity/StringOrRes.kt
@@ -7,22 +7,18 @@ import androidx.viewbinding.ViewBinding
 import com.nabla.sdk.core.annotation.NablaInternal
 import com.nabla.sdk.core.ui.helpers.context
 
-@NablaInternal
 public sealed interface StringOrRes {
     public data class String(val stringValue: kotlin.String) : StringOrRes
     public data class Res(@StringRes val resValue: Int) : StringOrRes
 }
 
-@NablaInternal
 public fun StringOrRes.evaluate(context: Context): String = when (this) {
     is StringOrRes.String -> stringValue
     is StringOrRes.Res -> context.getString(resValue)
 }
 
-@NablaInternal
 public fun StringOrRes.evaluate(fragment: Fragment): String = evaluate(fragment.requireContext())
 
-@NablaInternal
 public fun StringOrRes.evaluate(viewBinding: ViewBinding): String = evaluate(viewBinding.context)
 
 @NablaInternal
